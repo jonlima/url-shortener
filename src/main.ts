b073d8dfbdfaa515cccd36e8bd50511726e543ff
Jonatha Lima <jonatha.lima14@gmail.com>
2024-08-17 22:39:47 -0300

    Add.env.example file and environment variable configuration.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config'
+
+const PORT_DEFAULT = 8000;
+const DOMAIN_DEFAULT = 'http://localhost';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = app.get<ConfigService>(ConfigService);
+  const port = config.get<number>('APP_PORT') || PORT_DEFAULT;
+  const domain = config.get<number>('API_DOMAIN') || DOMAIN_DEFAULT;
+
+  await app.listen(port, () => {
+    const msg = `Server is running at ${domain}:${port}`
+    console.log(msg)
+  });
 }
 bootstrap();
